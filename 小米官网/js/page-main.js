ae806a2b75ed5eadc0c67a36277caf86f3c0a40e
sunweixin <359561000@qq.com>
2018-04-14 18:54:21 +0800

    小米官网

--- 小米官网/js/page-main.js
+++ 小米官网/js/page-main.js
@@ -2,12 +2,14 @@
  * Created by sunweixin on 2018/4/9.
  */
 $(function(){
-   $('.page-main .span16  img').on('mouseover',function(){
-       $('.page-main .popups').css('display','block');
-       $('.page-main .popups').height('80px');
-       console.log($(this));
+   $('.page-main .span16 .brick-items').on('mouseover',function(){
+
+       // $('.page-main .popups').css('display','block');
+       // $('.page-main .popups').height('80px');
+       $(this).find('.popups').css('display','block');
+       $(this).find('.popups').height('80px');
    });
-    $('.page-main .span16  img').on('mouseleave',function(){
+    $('.page-main .span16 .brick-items').on('mouseleave',function(){
         $('.page-main .popups').height('0');
         $('.page-main .popups').css('display','none');
     });
